67d92ef79cbbaa246e389a396447b35ab957ba77
Rhyzhang <ryanzhangofficial@gmail.com>
2026-01-24 01:16:16 -0500

    udpate

--- app/(pages)/team/page.tsx
+++ app/(pages)/team/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 import Hero from '~/components/widgets/Hero';
 import Team from '~/components/widgets/Team';
-import { heroTeam, teamHome } from '~/shared/data/pages/team.data';
+import { teamHome } from '~/shared/data/pages/team.data';
 
 export const metadata: Metadata = {
   title: 'Our Team',
