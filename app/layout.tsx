cbdaf8c9016a00a2c0223a19251a1cf5475da7e2
Rhyzhang <ryanzhangofficial@gmail.com>
2026-01-24 20:30:05 -0500

    darkmode fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
+    <html lang="en" className={`dark motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="tracking-tight antialiased text-gray-900 dark:text-slate-300 dark:bg-slate-900">
+      <body className="tracking-tight antialiased bg-slate-900 text-slate-300">
         <Providers>
           {/* <Announcement /> Remove for now but can add back by uncommenting */}
           <Header />
--- src/components/atoms/Providers.tsx
+++ src/components/atoms/Providers.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import { ThemeProvider } from 'next-themes';
-
 export interface ProvidersProps {
   children: React.ReactNode
 }
 
-const Providers = ({ children }: ProvidersProps) => (
-  <ThemeProvider attribute="class" disableTransitionOnChange>
-    {children}
-  </ThemeProvider>
-);
+const Providers = ({ children }: ProvidersProps) => <>{children}</>;
 
 export default Providers;
